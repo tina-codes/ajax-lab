52c4d13bc907d8a1b2c737d25fc2e507a52e695a
Christina <you@example.comchristinanicosia08@gmail.comgit config --global user.name Your>
2022-10-12 12:40:06 -0700

    showWeather

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -20,8 +20,14 @@ function showWeather(evt) {
 
   const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
+  const queryUrl = `${url}?${zipcode}`;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(queryUrl)
+    .then((response) => response.json())
+    .then((weather_info) => {
+      document.querySelector('#weather-info').innerHTML = weather_info['forecast'];
+    });
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
